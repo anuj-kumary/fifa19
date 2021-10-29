8349fcec607129cd7e16d14f702d22c3fea007c7
anuj-kumary <anujf0510@gmail.com>
2021-10-29 08:16:30 +0530

    formatting code

--- script.js
+++ script.js
@@ -16,80 +16,78 @@ const clubName = document.querySelector("#club-name");
 const home = document.querySelector("#home")
 
 function hideData() {
-  search.style.display = "none";
-  searchPlayer.style.display = "none";
-  output.style.display = "block";
+    search.style.display = "none";
+    searchPlayer.style.display = "none";
+    output.style.display = "block";
 }
 
 const getPlayerData = function () {
-  hideData();
-  let searchName = searchPlayer.value;
-  fetch("./player.json")
-    .then((response) => response.json())
-    .then((data) => {
-      data.map((player) => {
-        if (player.Name === searchName) {
-          age.innerText = player.Age;
-          height.innerText = player.Height;
-          weight.innerText = player.Weight;
-          position.innerText = player.Position;
-          jerseyNumber.innerText = player["Jersey Number"];
-          preferredFoot.innerText = player["Preferred Foot"];
-          heading.innerText = player.Name;
-          workRate.innerText = player["Work Rate"];
-          value.innerText = player.Value;
-          clubName.innerText = player.Club;
+    hideData();
+    let searchName = searchPlayer.value;
+    fetch("./player.json")
+        .then((response) => response.json())
+        .then((data) => {
+            data.map((player) => {
+                if (player.Name === searchName) {
+                    age.innerText = player.Age;
+                    height.innerText = player.Height;
+                    weight.innerText = player.Weight;
+                    position.innerText = player.Position;
+                    jerseyNumber.innerText = player["Jersey Number"];
+                    preferredFoot.innerText = player["Preferred Foot"];
+                    heading.innerText = player.Name;
+                    workRate.innerText = player["Work Rate"];
+                    value.innerText = player.Value;
+                    clubName.innerText = player.Club;
 
-          const ctx = document.getElementById("myChart").getContext("2d");
-          let chartStatus = Chart.getChart("myChart");
-          if (chartStatus != undefined) {
-            chartStatus.destroy();
-          }
+                    const ctx = document.getElementById("myChart").getContext("2d");
+                    let chartStatus = Chart.getChart("myChart");
+                    if (chartStatus != undefined) {
+                        chartStatus.destroy();
+                    }
 
-          const myChart = new Chart(ctx, {
-            type: "radar",
-            data: {
-              label: 'Player Graph',
-              labels: [
-                "Crossing",
-                "Finishing",
-                "HeadingAccuracy",
-                "ShortPassing",
-                "Volleys",
-                "Overall",
-              ],
-              datasets: [
-                {
-                  label: 'Player Graph',
-                  data: [
-                    player.Crossing,
-                    player.Finishing,
-                    player.HeadingAccuracy,
-                    player.ShortPassing,
-                    player.Volleys,
-                    player.Overall,
-                  ],
-                  backgroundColor: ["rgba(255, 159, 64, 0.2)"],
-                  borderColor: [
-                    "#f24d67",
-                    "#f24d67",
-                    "#f24d67",
-                    "#f24d67",
-                    "#f24d67",
-                    "#f24d67",
-                  ],
-                  borderWidth: 1,
-                },
-              ],
-            },
-            options: {
-              responsive: true,
-              maintainAspectRatio: false,
-            },
-          });
-        }
-      });
-    });
+                    const myChart = new Chart(ctx, {
+                        type: "radar",
+                        data: {
+                            label: 'Player Graph',
+                            labels: [
+                                "Crossing",
+                                "Finishing",
+                                "HeadingAccuracy",
+                                "ShortPassing",
+                                "Volleys",
+                                "Overall",
+                            ],
+                            datasets: [{
+                                label: 'Player Graph',
+                                data: [
+                                    player.Crossing,
+                                    player.Finishing,
+                                    player.HeadingAccuracy,
+                                    player.ShortPassing,
+                                    player.Volleys,
+                                    player.Overall,
+                                ],
+                                backgroundColor: ["rgba(255, 159, 64, 0.2)"],
+                                borderColor: [
+                                    "#f24d67",
+                                    "#f24d67",
+                                    "#f24d67",
+                                    "#f24d67",
+                                    "#f24d67",
+                                    "#f24d67",
+                                ],
+                                borderWidth: 1,
+                            }, ],
+                        },
+                        options: {
+                            responsive: true,
+                            maintainAspectRatio: false,
+                        },
+                    });
+                }
+            });
+        });
 };
 
 search.addEventListener("click", getPlayerData);
